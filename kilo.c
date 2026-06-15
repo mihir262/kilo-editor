9786cf61eda31d3ff791c27b13d9c2698f50fca6
mihir <mihirdeogaonkar@gmail.com>
2026-06-15 15:26:12 +0530

    add welcome screen, center it and add padding

--- kilo.c
+++ kilo.c
@@ -11,7 +11,7 @@
 
 // defines
 
-
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 // data
@@ -126,7 +126,25 @@ void abFree(struct abuf *ab){
 void editorDrawRows(struct abuf *ab){
 	int y;
 	for(y = 0; y < E.screenrows; y++){
+		if(y == E.screenrows / 3){
+			char welcome[80];
+			
+			int welcomelen = snprintf(welcome, sizeof(welcome), "KILO EDTIOR - VERSION %s", KILO_VERSION);
+			
+			if(welcomelen > E.screencols) welcomelen = E.screencols;
+			int padding = (E.screencols - welcomelen) / 2;
+			
+			if(padding) {
+				abAppend(ab, "~", 1);
+				padding --;
+			}
+			
+			while (padding--) abAppend(ab, " ", 1);
+			abAppend(ab, welcome, welcomelen);
+		
+		} else {
 		abAppend(ab, "~", 1);
+		}
 
 		abAppend(ab,"\x1b[K", 3);
 		if(y < E.screenrows -1){
@@ -147,7 +165,7 @@ void editorRefreshScreen(){
 	abAppend(&ab, "\x1b[?25h", 6);
 
 	write(STDOUT_FILENO, ab.b, ab.len);
-	abFree(ab);
+	abFree(&ab);
 }
 
 // input
