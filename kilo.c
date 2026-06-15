b1fa6bd27049fe690b374e8cee495dd018ff00fd
mihir <mihirdeogaonkar@gmail.com>
2026-06-15 21:53:28 +0530

    add support for HOME, END, and DELETE keys in editor key handling

--- kilo.c
+++ kilo.c
@@ -20,7 +20,10 @@ enum editorKey {
 	ARROW_UP, 
 	ARROW_DOWN,
 	PAGE_UP,
-	PAGE_DOWN
+	PAGE_DOWN,
+	HOME_KEY,
+	END_KEY,
+	DELETE_KEY
 };
 
 // data
@@ -64,37 +67,56 @@ void enableRawMode(){
 }
 
 int editorReadKey() {
-  int nread;
-  char c;
-  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-    if (nread == -1 && errno != EAGAIN) die("read");
-  }
-  if (c == '\x1b') {
-    char seq[3];
-    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
-    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
-    if (seq[0] == '[') {
-      if (seq[1] >= '0' && seq[1] <= '9') {
-        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
-        if (seq[2] == '~') {
-          switch (seq[1]) {
-            case '5': return PAGE_UP;
-            case '6': return PAGE_DOWN;
-          }
-        }
-      } else {
-        switch (seq[1]) {
-          case 'A': return ARROW_UP;
-          case 'B': return ARROW_DOWN;
-          case 'C': return ARROW_RIGHT;
-          case 'D': return ARROW_LEFT;
-        }
-      }
+    int nread;
+    char c;
+
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
     }
-    return '\x1b';
-  } else {
-    return c;
-  }
+
+    if (c == '\x1b') {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+            return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+            return '\x1b';
+
+        if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+						case '1': return HOME_KEY;
+						case '3': return DELETE_KEY;
+						case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+						case '7': return HOME_KEY;
+						case '8': return END_KEY;
+                    }
+                }
+            } else {
+                switch (seq[1]) {
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+					case 'H': return HOME_KEY;
+					case 'F': return END_KEY;
+                }
+            }
+        } else if (seq[0] == 'O') {
+			switch (seq[1]) {
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY;
+			}
+		}
+
+        return '\x1b';
+    } else {
+        return c;
+    }
 }
 
 int getCursorPosition(int *rows, int *cols){
@@ -232,14 +254,19 @@ void editorProcessKeypress(){
 			exit(0);
 			break;
 
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+			
+		case END_KEY:
+			E.cx = E.screencols - 1;
+			break;
+
 		case PAGE_UP:
 		case PAGE_DOWN:
 			{
 				int times = E.screenrows;
-				while(times--){
-					editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
-				}
-
+				while(times--) editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
 			}
 			break;
 		case ARROW_UP:
